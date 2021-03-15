2e1c18d885842b683a59fb496487f4136626a805
authorizeleo <75794695+authorizeleo@users.noreply.github.com>
2021-03-15 08:05:09 +0800

    Add files via upload

--- Week4/week4_test.py
+++ Week4/week4_test.py
@@ -12,7 +12,6 @@ def index():
     if number:
         return redirect("/member/")
     else:
-        session.pop("number", None)
         return render_template("index.html")
 
 @app.route('/signin', methods=["POST","GET"])
