6c01f591c3b031a6f42b0ac0ababac26f66fc59c
authorizeleo <75794695+authorizeleo@users.noreply.github.com>
2021-04-05 14:22:41 +0800

    Add files via upload

--- Week7/week7_test.py
+++ Week7/week7_test.py
@@ -24,8 +24,17 @@ class User(db.Model):
 
 
 
-@app.route('/',methods=['POST','GET'])
+@app.route('/')
 def index():
+    name = session.get('name')
+    if name:
+        return redirect("/member/")
+    else:
+        return render_template("index.html")
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
     if request.method == 'POST':
         registername = request.form['name']
         registerusername = request.form['username']
@@ -41,12 +50,7 @@ def index():
         else:
             flash("欄位不得為空!")
             return redirect('/')
-            
-    name = session.get('name')
-    if name:
-        return redirect("/member/")
-    else:
-        return render_template("index.html")
+
 
 @app.route('/signin', methods=["POST","GET"])
 def signin():
@@ -56,35 +60,22 @@ def signin():
     if request.method == "POST":
         signinCheck = User.query.filter_by(username= numbers).first()
         if signinCheck is not None and numbers == signinCheck.username and password == signinCheck.password:
-            session["name"] = signinCheck.name
             session['id'] = signinCheck.id
             return  redirect("/member/") 
         else:
             return redirect(url_for("error", message="帳號或密碼輸入錯誤"))
-    name = session.get('name')
-    if name:
+    id = session.get('id')
+    if id:
         return redirect('/member/')
     else:
         return '<h1 style="color:lightpink">錯誤操作<h1> <br> <a href="/">返回首頁</a>'
 
-@app.route('/member/' , methods=['GET','POST'])
+@app.route('/member/' , methods=["POST","GET"])
 def member():
-    name = session.get('name')
     id = session.get('id')
-    if name: 
-        if request.method == 'POST':
-            updateName = request.form['updateName']
-            if updateName.strip() == "" : 
-                flash('名字不得為空')
-                return redirect('/member/')
-            SearchupdateName = User.query.filter_by(id=id).first()
-            SearchupdateName.name = updateName
-            session["name"] = SearchupdateName.name
-            db.session.commit()
-            flash('更新成功')
-            return render_template("member.html" , data=updateName)
-        else:
-            return render_template("member.html" , data=name)
+    databases = User.query.filter_by(id=id).first()
+    if id: 
+        return render_template("member.html" , data=databases.name)
     else :
         flash("拜託你，可以好好登入嗎?")
         return redirect('/')
@@ -94,8 +85,8 @@ def member():
 
 @app.route('/error/')
 def error():
-    name = session.get('name')
-    if name:
+    id = session.get('id')
+    if id:
         flash("千萬不要這樣子玩!")
         return redirect("/member/")
     else:
@@ -114,28 +105,43 @@ def loser():
 
 @app.route('/api/users')
 def api():
-    databases = User.query.all()
-    words = []
-    for data in databases:
-        word = {
-            "data":{
-                "id":data.id,
-                "name":data.name,
-                "username":data.username},
+    id = session.get('id')
+    if id :            
+        nulluser = {
+            "data": None
         }
-        words.append(word)
-        
-    nulluser = {
-        "data": None
-    }
-    if 'username' in request.args:
-        username = request.args['username']
+        if 'username' in request.args:
+            username = request.args['username']
+        else:
+            return jsonify(nulluser)
+        databases = User.query.filter_by(username=username).first()
+        if databases is None:
+            return jsonify(nulluser)
+        else:
+            users ={
+                "data":{
+                    "id":databases.id,
+                    "name":databases.name,
+                    "username":databases.username
+                }
+            }
+            return jsonify(users)  
     else:
-        return jsonify(nulluser)
-    for w in words:
-        if w['data']["username"] == username:
-            return jsonify(w)
-    return jsonify(nulluser)
+        flash('沒有登入唷')
+        return redirect('/')
+
+@app.route('/api/user', methods=['POST',"GET"])
+def updatename():
+    id = session.get('id')
+    if id:
+        databases = User.query.filter_by(id=id).first()
+        updateName = request.json['name']
+        databases.name = updateName
+        db.session.commit()
+        session["name"] = updateName
+        return render_template('member.html')
+    else:
+        return redirect("/")
 
 
    
